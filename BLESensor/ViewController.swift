37afcebfabee37cb9692b3cb5a05acc31b83b86d
Aydin Anliacik <aydin.anliacik@icloud.com>
2019-08-12 17:45:20 +0300

    Initial Commit

---
+++ BLESensor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLESensor
+//
+//  Created by Aydın Anlıaçık on 12.08.2019.
+//  Copyright © 2019 Aydın Anlıaçık. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
